9e11d0f83e61eac1ac10e8576eb8b7b397351268
yannis971 <yannis.saliniere@gmail.com>
2021-01-15 12:19:39 +0100

    version 0.0.2 ajout templates et models.py

--- jsiapp/views.py
+++ jsiapp/views.py
@@ -1,9 +1,17 @@
 
 # -*-coding:utf-8 -*
-from flask import Flask
+from flask import Flask, render_template
+
+from .models import Movie
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello world !"
+    best_movie = Movie.the_best_imdb_score()
+    bests_movies = Movie.bests_imdb_score()
+    movies_by_genre = list() 
+    movies_by_genre[0] = Movie.bests_imdb_score_by_genre("Action")
+    movies_by_genre[1] = Movie.bests_imdb_score_by_genre("Comedy")
+    movies_by_genre[2] = Movie.bests_imdb_score_by_genre("Sci-Fi")
+    return render_template('index.html', **locals())
